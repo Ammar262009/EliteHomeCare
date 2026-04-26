6a1dee27c0b45e731f4d72e0876c506461446d68
Suhail Ansari <tajmohdammar@gmail.com>
2026-04-26 16:49:15 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
 import React, { useState } from 'react'
 import './App.css'
+import Photo from './component/Photo'
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const App = () => {
+  const [photoArr, setPhotoArr] = useState([])
+  const [index, setIndex] = useState(1)
 
-  const [num, setNum] = useState(0)
+  const getData = async () => {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=14`)
+    setPhotoArr(response.data)
+  }
+  useEffect(() => {
+    getData()
+  }, [index])
 
-  const increase = () => {
-    setNum(num+1)
+  const increaseIndex = () => {
+    setIndex(index + 1)
+    setPhotoArr([])
   }
 
-  const decrease = () => {
-    setNum(num-1)
+  const decreaseIndex = () => {
+    if (index > 1) {
+      setIndex(index - 1)
+      setPhotoArr([])
+    }
+  }
+  let printData = <h3 className='top-1/2 left-1/2 -translate-1/2 absolute font-semibold text-2xl'>Loading...</h3>
+  if (photoArr.length > 0) {
+    {
+      printData = photoArr.map((elem, idx) => {
+        return <Photo elem={elem} key={idx} />
+      })
+    }
   }
 
   return (
-    <div className='h-screen w-full bg-gray-800  text-white'>
-      <h2 className='p-5'>Counter App</h2>
-      <div className='m-3 w-30 rounded text-center justify-center items-center flex h-30 bg-amber-400 text-black font-bold text-3xl'>{num}</div>
-      <div className='flex gap-3 p-6'>
-        <button onClick={decrease} className='bg-emerald-700 text-white px-5 rounded py-3'>Decrease</button>
-        <button onClick={increase} className='bg-emerald-700 text-white px-5 rounded py-3'>Increase</button>
+    <div className='h-screen w-full bg-black overflow-auto  text-white'>
+      <h1 className='pt-3 font-bold text-xl text-center'>Photo Gallery</h1>
+      <div className='py-7 flex justify-center items-center flex-wrap gap-2 px-4 my-4 h-auto min-h-[80%] w-full relative bg-amber-800'>
+        {printData}
+      </div>
+      <div className='flex gap-8 justify-center mb-4 items-center'>
+        <button style={{ opacity: index == 1 ? 0.5 : 1, cursor: index == 1 ? 'not-allowed' : 'pointer' }} onClick={decreaseIndex} className='bg-emerald-900 cursor-pointer active:scale-95 text-white px-5 py-2 rounded'>Prev</button>
+        <div>Page: {index}</div>
+        <button onClick={increaseIndex} className='bg-emerald-900 active:scale-95 text-white cursor-pointer px-5 py-2 rounded'>Next</button>
       </div>
     </div>
   )
